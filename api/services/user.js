096c88f0727fb2c5dae3feb76e374e940ae5a4db
seba-the-programmer <megaseba9@gmail.com>
2020-08-14 12:16:38 +0200

    css for email

--- api/services/user.js
+++ api/services/user.js
@@ -32,7 +32,41 @@ export const register = async (username, email, passRaw) => {
         from: 'rentalNetNoReply@gmail.com',
         to: email,
         subject: 'Confirm your email',
-        html: `Click link to confirm your email => <a href="${url}">Link</a>`
+        html: `
+                <body style="background-color: #f0d6b9;">
+                    <div style="
+                        align-items: center;
+                        text-align: center;
+                        width:100%;
+                        font-size: 1.5em;
+                        font-family: Arial, Helvetica, sans-serif;
+                        -webkit-font-smoothing: antialiased;
+                        -moz-osx-font-smoothing: grayscale;
+                        color:#852a37;">
+                        <div style="font-size: 2em;
+                        margin-top: 4vh;
+                        width: auto;
+                        background-color: crimson;
+                        color: #f2d5d7;
+                        margin-bottom:2vh;
+                        padding: 10px;">Hi, ${username}!</div>
+                        <div id="content">
+                            It's enormous pleasure to have you in our family!<br>
+                            Click button below to confirm your email and start using Rental Net website.
+                        </div>
+                        <a href="${url}"><button style="
+                        margin-top: 3vh;
+                        font-size: 1.5em;
+                        cursor: pointer;
+                        background-color: #f0515c;
+                        color: #f2d5d7;
+                        border: none;
+                        border-radius: 15px;
+                        padding: 5px;">Click me</button></a>
+                        <div style="margin-top: 3vh;
+                        color:darkgrey;">This message has been generated automatically. Please don't respond to it.</div>
+                    </div>
+                </body>`
     }
     transport.sendMail(message, function(err, info) {
         if (err) {
